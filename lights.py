665e5d2a843f64bfaade623a6882d1d7dc747f4b
anoushka <anoushka@ucsb.edu>
2023-12-02 14:36:02 -0800

    lights work with python script

--- lights.py
+++ lights.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+import pigpio
 import time
 
 # Set GPIO pins for R, G, B
@@ -6,46 +6,29 @@ PIN_R = 23
 PIN_G = 24
 PIN_B = 25
 
-# Set the GPIO mode to BCM
-GPIO.setmode(GPIO.BCM)
+# Connect to the pigpio daemon
+pi = pigpio.pi()
 
-# Set up PWM for each color
-GPIO.setup(PIN_R, GPIO.OUT)
-GPIO.setup(PIN_G, GPIO.OUT)
-GPIO.setup(PIN_B, GPIO.OUT)
-
-# Set PWM frequency (Hz)
-PWM_FREQ = 100
-
-# Create PWM instances for R, G, B
-pwm_r = GPIO.PWM(PIN_R, PWM_FREQ)
-pwm_g = GPIO.PWM(PIN_G, PWM_FREQ)
-pwm_b = GPIO.PWM(PIN_B, PWM_FREQ)
+# Set initial PWM values to 0
+pi.set_PWM_dutycycle(PIN_R, 0)
+pi.set_PWM_dutycycle(PIN_G, 0)
+pi.set_PWM_dutycycle(PIN_B, 0)
 
 def set_color(r, g, b):
-    # Normalize values to the range 0-100
-    r = max(0, min(100, r))
-    g = max(0, min(100, g))
-    b = max(0, min(100, b))
-
-    # Map normalized values to PWM duty cycles (0-100)
-    duty_cycle_r = (r / 100.0) * 100
-    duty_cycle_g = (g / 100.0) * 100
-    duty_cycle_b = (b / 100.0) * 100
+    # Normalize values to the range 0-255
+    r = max(0, min(255, r))
+    g = max(0, min(255, g))
+    b = max(0, min(255, b))
 
     # Set PWM duty cycles for R, G, B
-    pwm_r.ChangeDutyCycle(duty_cycle_r)
-    pwm_g.ChangeDutyCycle(duty_cycle_g)
-    pwm_b.ChangeDutyCycle(duty_cycle_b)
+    pi.set_PWM_dutycycle(PIN_R, 255 - r)
+    pi.set_PWM_dutycycle(PIN_G, 255 - g)
+    pi.set_PWM_dutycycle(PIN_B, 255 - b)
 
-# Example: Set color to Red (100% intensity)
 set_color(0, 0, 255)
 
 # Allow some time for the color to be visible (adjust as needed)
 time.sleep(2)
 
 # Clean up GPIO
-pwm_r.stop()
-pwm_g.stop()
-pwm_b.stop()
-GPIO.cleanup()
+pi.stop()
