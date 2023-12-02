9e6923bae169820f7cd38efbdc1d142b003f459d
anoushka <anoushka@ucsb.edu>
2023-12-02 14:20:27 -0800

    bash script and new python file

--- lights.py
+++ lights.py
@@ -1,93 +1,51 @@
-import usb.core
-import usb.util
+import RPi.GPIO as GPIO
 import time
 
-# Define USB device parameters
-VENDOR_ID = 0x0424  # Replace with your USB device's vendor ID
-PRODUCT_ID = 0x9514  # Replace with your USB device's product ID
-INTERFACE = 0
+# Set GPIO pins for R, G, B
+PIN_R = 23
+PIN_G = 24
+PIN_B = 25
 
-# USB control request parameters
-REQUEST_TYPE = usb.util.CTRL_OUT | usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_RECIPIENT_DEVICE
-REQUEST = 0x09
-VALUE = 0x0200
-INDEX = 0x00
-print("finding now")
-# Find the USB device
-dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
 
-if dev is None:
-    raise ValueError("USB device not found.")
+# Set up PWM for each color
+GPIO.setup(PIN_R, GPIO.OUT)
+GPIO.setup(PIN_G, GPIO.OUT)
+GPIO.setup(PIN_B, GPIO.OUT)
 
-try:
-    print("dev: ", dev)
+# Set PWM frequency (Hz)
+PWM_FREQ = 100
 
-    # Detach the kernel driver if active
-    if dev.is_kernel_driver_active(INTERFACE):
-        dev.detach_kernel_driver(INTERFACE)
+# Create PWM instances for R, G, B
+pwm_r = GPIO.PWM(PIN_R, PWM_FREQ)
+pwm_g = GPIO.PWM(PIN_G, PWM_FREQ)
+pwm_b = GPIO.PWM(PIN_B, PWM_FREQ)
 
-    # Set the active configuration. The device may have multiple configurations.
-    dev.set_configuration()
+def set_color(r, g, b):
+    # Normalize values to the range 0-100
+    r = max(0, min(100, r))
+    g = max(0, min(100, g))
+    b = max(0, min(100, b))
 
-    # Access the USB interface
-    usb.util.claim_interface(dev, INTERFACE)
+    # Map normalized values to PWM duty cycles (0-100)
+    duty_cycle_r = (r / 100.0) * 100
+    duty_cycle_g = (g / 100.0) * 100
+    duty_cycle_b = (b / 100.0) * 100
 
-    # Example: Turn on the LED
-    print("Turning on the LED...")
-    dev.ctrl_transfer(REQUEST_TYPE, REQUEST, VALUE, INDEX, b"\x01")
+    # Set PWM duty cycles for R, G, B
+    pwm_r.ChangeDutyCycle(duty_cycle_r)
+    pwm_g.ChangeDutyCycle(duty_cycle_g)
+    pwm_b.ChangeDutyCycle(duty_cycle_b)
 
-    # Wait for a moment
-    time.sleep(2)
+# Example: Set color to Red (100% intensity)
+set_color(0, 0, 255)
 
-    # Example: Turn off the LED
-    print("Turning off the LED...")
-    dev.ctrl_transfer(REQUEST_TYPE, REQUEST, VALUE, INDEX, b"\x00")
+# Allow some time for the color to be visible (adjust as needed)
+time.sleep(2)
 
-finally:
-    # Release the USB interface
-    usb.util.release_interface(dev, INTERFACE)
-
-    # Attach the kernel driver if it was detached
-    dev.attach_kernel_driver(INTERFACE) if dev.is_kernel_driver_active(INTERFACE) else None
-
-
-# import RPi.GPIO as GPIO
-# import time
-
-# # Pin configuration - Modify based on your actual setup
-# red_pin = 27    # GPIO pin connected to the red channel of the RGB LED strip
-# green_pin = 17  # GPIO pin connected to the green channel of the RGB LED strip
-# blue_pin = 22   # GPIO pin connected to the blue channel of the RGB LED strip
-
-# # Setup GPIO
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(red_pin, GPIO.OUT)
-# GPIO.setup(green_pin, GPIO.OUT)
-# GPIO.setup(blue_pin, GPIO.OUT)
-
-# # Helper function to set the color
-# def set_color(red_value, green_value, blue_value):
-#     GPIO.output(red_pin, red_value)
-#     GPIO.output(green_pin, green_value)
-#     GPIO.output(blue_pin, blue_value)
-
-# try:
-#     while True:
-#         # Red
-#         print("Red")
-#         set_color(GPIO.HIGH, GPIO.LOW, GPIO.LOW)  # Turn on red, turn off others
-#         time.sleep(1)
-
-#         # Green
-#         print("Green")
-#         set_color(GPIO.LOW, GPIO.HIGH, GPIO.LOW)  # Turn on green, turn off others
-#         time.sleep(1)
-
-#         # Blue
-#         print("Blue")
-#         set_color(GPIO.LOW, GPIO.LOW, GPIO.HIGH)  # Turn on blue, turn off others
-#         time.sleep(1)
-
-# except KeyboardInterrupt:
-#     print("\nExiting...")
-#     GPIO.cleanup()  # Clean up GPIO on Ctrl+C exit
+# Clean up GPIO
+pwm_r.stop()
+pwm_g.stop()
+pwm_b.stop()
+GPIO.cleanup()
